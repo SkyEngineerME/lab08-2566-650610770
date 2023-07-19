e7ae919ab77f941304f23873d4c956a051b8f0ee
Thanathorn <guymechatronic@gmail.com>
2023-07-19 10:59:57 +0700

    Done

---
+++ component/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = (year, fullname, studentId) => {
+  <div className="mt-3">
+    <p className="text-secondary text-center">
+      copyright © {year} {fullname} {studentId}
+    </p>
+  </div>;
+};
